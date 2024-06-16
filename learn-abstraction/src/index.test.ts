2e4f239d09e59902ab5971a22086d5ad9c98116e
Eriec Tanijaya <eriectan88@gmail.com>
2024-06-17 01:36:15 +0700

    feat: user should have name

--- learn-abstraction/src/index.test.ts
+++ learn-abstraction/src/index.test.ts
@@ -2,8 +2,16 @@ import { User } from '.';
 
 describe('User', () => {
   test('should able to create user', () => {
-    const user = new User();
+    const user = new User('hehe');
 
     expect(user).toBeInstanceOf(User);
+  });
+
+  test('should have name', () => {
+    const name = 'Lala';
+
+    const user = new User(name);
+
+    expect(user.name).toBe(name);
   });
 });
--- learn-abstraction/src/index.ts
+++ learn-abstraction/src/index.ts
@@ -1,1 +1,7 @@
-export class User {}
+export class User {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
